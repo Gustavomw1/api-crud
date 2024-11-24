7b7a909bb0cfa5004eff37bca7621f93b6c7ce35
Gustavo <gustavo303a@gmail.com>
2024-11-24 14:16:06 -0300

    tentando fazer deploy

--- dist/public/logar/script.js
+++ dist/public/logar/script.js
@@ -1,46 +1,54 @@
-"use strict";
 const API_URL = "http://localhost:3000";
+
 // Função para realizar login do usuário
 async function loginUser() {
-    const email = document.getElementById("email").value.trim();
-    const senha = document.getElementById("senha").value.trim();
-    // Verifica se os campos estão preenchidos
-    if (!email || !senha) {
-        showMessage("Por favor, preencha todos os campos!", "error");
-        return;
+  const email = document.getElementById("email").value.trim();
+  const senha = document.getElementById("senha").value.trim();
+
+  // Verifica se os campos estão preenchidos
+  if (!email || !senha) {
+    showMessage("Por favor, preencha todos os campos!", "error");
+    return;
+  }
+
+  //logar
+  try {
+    const response = await fetch(`${API_URL}/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, senha }),
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+      showMessage("Login realizado com sucesso! Redirecionando...", "success");
+
+      setTimeout(() => {
+        window.location.href = "";
+      }, 3000);
+    } else {
+      showMessage(
+        data.mensagem || "Erro ao realizar login. Tente novamente.",
+        "error"
+      );
     }
-    //logar
-    try {
-        const response = await fetch(`${API_URL}/login`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ email, senha }),
-        });
-        const data = await response.json();
-        if (response.ok) {
-            showMessage("Login realizado com sucesso! Redirecionando...", "success");
-            setTimeout(() => {
-                window.location.href = "";
-            }, 3000);
-        }
-        else {
-            showMessage(data.mensagem || "Erro ao realizar login. Tente novamente.", "error");
-        }
-    }
-    catch (error) {
-        console.error("Erro ao realizar login:", error);
-        showMessage("Erro ao realizar login. Tente novamente mais tarde.", "error");
-    }
+  } catch (error) {
+    console.error("Erro ao realizar login:", error);
+    showMessage("Erro ao realizar login. Tente novamente mais tarde.", "error");
+  }
 }
+
 // Função para exibir mensagens no messageBox
 function showMessage(message, type) {
-    const messageBox = document.getElementById("messageBox");
-    messageBox.textContent = message;
-    messageBox.style.display = "block";
-    messageBox.style.backgroundColor = type === "success" ? "#4caf50" : "#f44336";
-    setTimeout(() => {
-        messageBox.style.display = "none";
-    }, 3000);
+  const messageBox = document.getElementById("messageBox");
+  messageBox.textContent = message;
+  messageBox.style.display = "block";
+  messageBox.style.backgroundColor = type === "success" ? "#4caf50" : "#f44336";
+
+  setTimeout(() => {
+    messageBox.style.display = "none";
+  }, 3000);
 }
--- dist/server.js
+++ dist/server.js
@@ -6,10 +6,17 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const routes_1 = __importDefault(require("./app/routes/routes"));
+const path_1 = __importDefault(require("path"));
 const app = (0, express_1.default)();
+// Middleware
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
+// Serve arquivos estáticos da pasta "public"
+const publicPath = path_1.default.resolve(__dirname, "public");
+app.use(express_1.default.static(publicPath));
+// Rotas
 app.use(routes_1.default);
+// Inicia o servidor
 app.listen(3000, () => {
     console.log("Server ok");
 });
