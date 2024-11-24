61c1cfc1b6f44102fef2d768240c8ba4574f71f3
Gustavo <gustavo303a@gmail.com>
2024-11-24 14:24:08 -0300

    tentando fazer deploy

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,15 @@
 import express from "express";
 import cors from "cors";
 import routers from "./app/routes/routes";
-import path from "path";
 
 const app = express();
 
-// Middleware
 app.use(cors());
+
 app.use(express.json());
 
-// Serve arquivos estáticos da pasta "public"
-const publicPath = path.resolve(__dirname, "public");
-app.use(express.static(publicPath));
-
-// Rotas
 app.use(routers);
 
-// Inicia o servidor
 app.listen(3000, () => {
   console.log("Server ok");
 });
